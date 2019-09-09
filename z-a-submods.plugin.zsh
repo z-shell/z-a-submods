19a5f408b9710105cc23c14bc2697234ddab7186
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-09-09 22:32:26 +0200

    Rename z-p-submods -> z-a-submods after Zplugin's z-plugins -> annexes

--- z-a-submods.plugin.zsh
+++ z-a-submods.plugin.zsh
@@ -2,14 +2,15 @@
 # http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html#zero-handling
 
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
 
 # Handler's are using the ":" symbol as the namespacer
 autoload -Uz :zp-submods-atclone-handler :zp-submods-atpull-handler \
         :zp-submods-atclone-help-handler :zp-submods-atpull-help-handler
 
-@zplg-register-z-plugin "z-p-submods" hook:atclone \
+@zplg-register-annex "z-a-submods" hook:atclone \
         :zp-submods-atclone-handler :zp-submods-atclone-help-handler \
         "submods''" # register a new ice-mod: submods''
 
-@zplg-register-z-plugin "z-p-submods" hook:%atpull \
+@zplg-register-annex "z-a-submods" hook:%atpull \
         :zp-submods-atpull-handler :zp-submods-atpull-help-handler
