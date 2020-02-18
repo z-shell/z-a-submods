88bfb2031767465bb314692d1f216dae496c190e
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-02-18 03:19:17 +0100

    Rename the handlers from →zp-* to →za-*

--- z-a-submods.plugin.zsh
+++ z-a-submods.plugin.zsh
@@ -5,12 +5,12 @@
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 # Handler's are using the ":" symbol as the namespacer
-autoload -Uz :zp-submods-atclone-handler :zp-submods-atpull-handler \
-        :zp-submods-atclone-help-handler :zp-submods-atpull-help-handler
+autoload -Uz →za-submods-atclone-handler →za-submods-atpull-handler \
+        →za-submods-atclone-help-handler →za-submods-atpull-help-handler
 
 @zinit-register-annex "z-a-submods" hook:atclone \
-        :zp-submods-atclone-handler :zp-submods-atclone-help-handler \
+        →za-submods-atclone-handler →za-submods-atclone-help-handler \
         "submods''" # register a new ice-mod: submods''
 
 @zinit-register-annex "z-a-submods" hook:%atpull \
-        :zp-submods-atpull-handler :zp-submods-atpull-help-handler
+        →za-submods-atpull-handler →za-submods-atpull-help-handler
