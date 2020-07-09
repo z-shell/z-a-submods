5ef54e4027c09cdb07499cf8d9a3f2a95f693450
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-07-09 07:28:27 +0200

    Impose order on the calling of hooks regardless of its registration.

--- z-a-submods.plugin.zsh
+++ z-a-submods.plugin.zsh
@@ -8,9 +8,9 @@
 autoload -Uz →za-submods-atclone-handler →za-submods-atpull-handler \
         →za-submods-atclone-help-handler →za-submods-atpull-help-handler
 
-@zinit-register-annex "z-a-submods" hook:atclone \
+@zinit-register-annex "z-a-submods" hook:atclone-30 \
         →za-submods-atclone-handler →za-submods-atclone-help-handler \
         "submods''" # register a new ice-mod: submods''
 
-@zinit-register-annex "z-a-submods" hook:%atpull \
+@zinit-register-annex "z-a-submods" hook:%atpull-30 \
         →za-submods-atpull-handler →za-submods-atpull-help-handler
