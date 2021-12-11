aaf288976e1b6038c801698d2e6f2f479ceff337
Salvydas Lukosius <sall@w-ss.io>
2021-12-11 11:51:47 +0000

    modified:   .editorconfig 	modified:   z-a-submods.plugin.zsh 	modified:   "\342\206\222za-submods-atclone-handler" 	modified:   "\342\206\222za-submods-atclone-help-handler" 	modified:   "\342\206\222za-submods-atpull-handler" 	modified:   "\342\206\222za-submods-atpull-help-handler"

--- z-a-submods.plugin.zsh
+++ z-a-submods.plugin.zsh
@@ -1,16 +1,16 @@
 # Standarized $0 handling, see:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html#zero-handling
+# https://github.com/z-shell/zi/wiki/Zsh-Plugin-Standard#1-standardized-0-handling
 
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 # Handler's are using the ":" symbol as the namespacer
 autoload -Uz →za-submods-atclone-handler →za-submods-atpull-handler \
-        →za-submods-atclone-help-handler →za-submods-atpull-help-handler
+→za-submods-atclone-help-handler →za-submods-atpull-help-handler
 
 @zi-register-annex "z-a-submods" hook:atclone-30 \
-        →za-submods-atclone-handler →za-submods-atclone-help-handler \
-        "submods''" # register a new ice-mod: submods''
+→za-submods-atclone-handler →za-submods-atclone-help-handler \
+"submods''" # register a new ice-mod: submods''
 
 @zi-register-annex "z-a-submods" hook:%atpull-30 \
-        →za-submods-atpull-handler →za-submods-atpull-help-handler
+→za-submods-atpull-handler →za-submods-atpull-help-handler
