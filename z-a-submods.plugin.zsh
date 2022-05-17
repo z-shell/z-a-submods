185e5dffd18be0187cab2b8b5acc1fb07b48c9f2
Salvydas Lukosius <sall@w-ss.io>
2022-05-17 19:10:56 +0100

    Update z-a-submods.plugin.zsh

--- z-a-submods.plugin.zsh
+++ z-a-submods.plugin.zsh
@@ -1,3 +1,6 @@
+# -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+# vim: ft=zsh sw=2 ts=2 et
+#
 # Standarized $0 handling
 # https://z.digitalclouds.dev/community/zsh_plugin_standard#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
@@ -16,10 +19,14 @@ autoload -Uz →za-submods-atclone-handler \
 →za-submods-atclone-help-handler \
 →za-submods-atpull-help-handler
 
+# An empty stub to fill the help handler fields
+→za-submods-null-handler() { :; }
+
 @zi-register-annex "z-a-submods" hook:atclone-30 \
   →za-submods-atclone-handler \
-  →za-submods-atclone-help-handler "submods''" # register a new ice-mod: submods''
+  →za-submods-atclone-help-handler \
+  →za-submods-null-handler "submods''" # register a new ice-mod: submods''
 
 @zi-register-annex "z-a-submods" hook:%atpull-30 \
   →za-submods-atpull-handler \
-  →za-submods-atpull-help-handler
+  →za-submods-null-handler
