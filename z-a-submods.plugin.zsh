30630f89a2263207785653386fe2110a3e7990d4
Salvydas Lukosius <sall@w-ss.io>
2022-10-30 14:49:31 +0000

    Update z-a-submods.plugin.zsh
    Signed-off-by: Salvydas Lukosius <sall@w-ss.io>

--- z-a-submods.plugin.zsh
+++ z-a-submods.plugin.zsh
@@ -2,18 +2,18 @@
 # vim: ft=zsh sw=2 ts=2 et
 #
 # Standarized $0 handling
-# https://z.digitalclouds.dev/community/zsh_plugin_standard#zero-handling
+# https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 # Functions directory
-# https://z.digitalclouds.dev/community/zsh_plugin_standard#funtions-directory
+# https://wiki.zshell.dev/community/zsh_plugin_standard#funtions-directory
 if [[ $PMSPEC != *f* ]] {
     fpath+=( "${0:h}/functions" )
 }
 
 # The Proposed Function-Name Prefixes
-# https://z.digitalclouds.dev/community/zsh_plugin_standard#the-proposed-function-name-prefixes
+# https://wiki.zshell.dev/community/zsh_plugin_standard#the-proposed-function-name-prefixes
 autoload -Uz →za-submods-atclone-handler \
 →za-submods-atpull-handler \
 →za-submods-atclone-help-handler
